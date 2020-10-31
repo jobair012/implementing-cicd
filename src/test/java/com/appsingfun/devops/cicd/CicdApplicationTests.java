2389807428f0e7d5de6424674ccb2d5fd18a1937
Jobair Hossain <jobair@nuarca.com>
2020-10-31 20:05:20 +0600

    initial commit

---
+++ src/test/java/com/appsingfun/devops/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsingfun.devops.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
