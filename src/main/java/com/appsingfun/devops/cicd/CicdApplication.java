78ca4641c01bbb327534b2007764add77004e0f6
Jobair Hossain <jobair@nuarca.com>
2020-10-31 21:47:57 +0600

    revert

--- src/main/java/com/appsingfun/devops/cicd/CicdApplication.java
+++ src/main/java/com/appsingfun/devops/cicd/CicdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdApplication {
 
 	public static void main(String[] args) {
-		SpringAcation.run(CicdApdadsasdfasdfasdfasdfasdplication.class, args);
+		SpringApplication.run(CicdApplication.class, args);
 	}
 
 }
